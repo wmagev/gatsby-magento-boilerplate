e5b888eecf08db0ccc61aac43802d52d4b03449d
willie <willie.lee1020@outlook.com>
2020-12-25 14:00:52 -0500

    Fix ignorecase

---
+++ src/components/Header/BusinessHours.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "styled-components"
+
+const BusinessHours = () => {
+    return (
+        <BusinessHoursWrapper>
+            SHOWROOM IS OPEN TO THE PUBLIC 9am - 5pm 7 DAYS- PLEASE BE SAFE AND PRACTICE SOCIAL DISTANCING
+        </BusinessHoursWrapper>
+    )
+}
+const BusinessHoursWrapper = styled.p`
+    text-align: center;
+    color: #fff;
+    font-size: 20px;
+    padding: 5px 0 10px 0;
+    margin: 0;
+`
+
+export default BusinessHours
---
+++ src/components/Header/NavBar/index.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import { THEME_SETTING } from "../../../constants"
+import { useCategoryData } from "../../../hooks/use-category-data"
+
+const NavBar = () => {
+
+    const categories = useCategoryData()
+
+    const renderNavItem = ({ node: item }) => {
+        return (
+            <NavBarItem key={item.magento_id}>
+                <Link to={item.url_path}>
+                    {item.name}
+                </Link>
+            </NavBarItem>
+        )
+    }
+
+    return (
+        <NavBarWrapper>
+            <NavBarLinks>
+                { categories && categories.map( renderNavItem ) }
+            </NavBarLinks>
+        </NavBarWrapper>
+    )
+}
+
+const NavBarWrapper = styled.div`
+    background: ${THEME_SETTING.NAV_BAR.BG_COLOR};
+    color: #fff;
+    text-align: center;
+`
+
+const NavBarLinks = styled.ul`
+    list-style: none;
+    margin: 0;
+`
+
+const NavBarItem = styled.li`
+    margin: 0 9px 0 0;
+    display: inline-block;
+    position: relative;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 55px;
+    padding: 0 12px;
+    text-align: left;
+    text-transform: uppercase;
+    &:hover {
+        background: ${THEME_SETTING.HOVER_COLOR};
+        cursor: pointer;
+    }
+    a {
+        text-decoration: none;
+        color: #fff;
+    }
+`
+
+export default NavBar
